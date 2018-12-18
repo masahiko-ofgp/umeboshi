5907a8568cc609458c1c05b09d4e451a86d91612
Masahiko Hamazawa <ichigyo.zanmai@gmail.com>
2018-12-18 11:12:14 +0900

    Add new screenshot and some commands.

--- README.md
+++ README.md
@@ -9,6 +9,15 @@ This application is my interactive shell.
 However, this application has just started to develop, 
 and does not have any particular functions.
 
+Now, umeboshi has four commands.
+
+- echo
+- quit
+- help
+- version
+
 ## Example
 
     $ cargo run --bin example
+
+<img src="./imgs/screenshot-umeboshi2.png" alt="screenshot">
--- src/shell.rs
+++ src/shell.rs
@@ -2,8 +2,15 @@ use std::io::Write;
 use std::io;
 use termion::{color, style};
 
-
+const VERSION: &str = "0.1.0";
 const PRIMARY_PROMPT: &str = "umeboshi>>";
+const HELP: &str = r#"
+    [Usage]
+    quit:                close shell.
+    % help or % -h:      help.
+    % version or % -v:   version information.
+    % echo [text]:       output string.
+"#;
 
 /// Main Loop 
 /// quit => Close shell.
@@ -20,13 +27,33 @@ pub fn main_loop() {
         io::stdout().flush().expect("Couldn't flush stdout");
         io::stdin().read_line(&mut s).expect("Failed.");
 
-        if s.starts_with("quit") {
+        let v: Vec<&str> = s.trim().split_whitespace().collect();
+
+        if &v[0] == &"quit" {
             println!("Bye!!");
             break;
+        } else if &v[0] == &"%" {
+            bind_func(&v);
+            continue;
         } else {
-            let line = s.trim();
-            println!("{}", line);
+            println!(
+                "\t{}Please input {} % help{}", 
+                color::Fg(color::LightYellow), 
+                PRIMARY_PROMPT, 
+                style::Reset
+            );
             continue;
         }
     }
 }
+
+fn bind_func<'b>(v: &Vec<&'b str>) {
+    match &v[1] {
+        &"echo" => println!("{:?}", &v[2..].join(&" ")),
+        &"help"|&"-h" => println!("{}{}{}", color::Fg(color::Cyan), HELP, style::Reset),
+        &"version"|&"-v" => println!("{}", VERSION),
+        _ => println!("Not exist its command."),
+    }
+}
+
+
